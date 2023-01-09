d7e333200503954331226edab4b9357bf3e960f6
Tolik Dem <tolik.demianov@gmail.com>
2023-01-09 14:02:38 +0300

    Пример с использованием псевдо-массива arguments

--- src/functions.ts
+++ src/functions.ts
@@ -22,3 +22,24 @@ function showName(firstName: string, lastName: string, ...titles: string[]) {
 }
 
 showName("Юлий", "Цезарь", "Консул", "Император");
+
+class MyClass {
+
+    // Все аргументы функции находятся в псевдомассиве arguments под своими порядковыми номерами
+    showName() {
+        console.log('using pseudo array arguments: ', arguments.length );
+        console.log('using pseudo array arguments: ', arguments[0] );
+        console.log('using pseudo array arguments: ', arguments[1] );
+
+        // Объект arguments можно перебирать
+        // for (let arg of arguments) alert(arg);
+    }
+}
+
+const myClass = new MyClass();
+
+// Вывод: 2, Юлий, Цезарь
+myClass.showName("Юлий", "Цезарь");
+
+// Вывод: 1, Илья, undefined (второго аргумента нет)
+myClass.showName("Илья");
