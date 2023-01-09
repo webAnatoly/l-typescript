a05a496a46a0ff5f44f652421c760429da7a85b7
Tolik Dem <tolik.demianov@gmail.com>
2023-01-09 13:56:32 +0300

    Еще пример функции с остаточными параметрами

--- src/functions.ts
+++ src/functions.ts
@@ -9,3 +9,16 @@ function sumAll(...args: number[]): number { // args — имя массива
 console.log( sumAll(1) ); // 1
 console.log( sumAll(1, 2) ); // 3
 console.log( sumAll(1, 2, 3) ); // 6
+
+
+function showName(firstName: string, lastName: string, ...titles: string[]) {
+    console.log( firstName + ' ' + lastName ); // Юлий Цезарь
+
+    // Оставшиеся параметры пойдут в массив
+    // titles = ["Консул", "Император"]
+    console.log('name 0: ', titles[0] ); // Консул
+    console.log('name 1: ', titles[1] ); // Император
+    console.log('name 2: ', titles.length ); // 2
+}
+
+showName("Юлий", "Цезарь", "Консул", "Император");
